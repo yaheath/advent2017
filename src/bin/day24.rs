7b07ca833cf7e099e4634f333ff0d82997aad002
Heath Kehoe <yaheath@gmail.com>
2023-03-06 16:52:25 -0600

    finished

---
+++ src/bin/day24.rs
@@ -0,0 +1,119 @@
+use std::collections::HashSet;
+use std::str::FromStr;
+use std::vec::Vec;
+use advent_lib::read::read_input;
+
+#[derive(Clone, Hash, Eq, PartialEq)]
+struct Component {
+    a: u64,
+    b: u64,
+}
+
+impl FromStr for Component {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut splt = s.split('/');
+        let a = splt.next().unwrap().parse::<u64>().unwrap();
+        let b = splt.next().unwrap().parse::<u64>().unwrap();
+        Ok(Component{a, b})
+    }
+}
+
+#[derive(Clone, Hash, Eq, PartialEq)]
+struct Bridge(Vec<Component>);
+
+impl Bridge {
+    fn new(c: &Component) -> Self {
+        assert!(c.a == 0 || c.b == 0);
+        Bridge(vec![c.clone()])
+    }
+    fn maybe_append(&self, c: &Component) -> Option<Self> {
+        if self.0.contains(c) {
+            // this component already part of the bridge
+            return None;
+        }
+        let idx = self.0.len() - 1;
+        let free =
+            if idx > 0 {
+                if self.0[idx - 1].a == self.0[idx].a || self.0[idx - 1].b == self.0[idx].a {
+                    self.0[idx].b
+                }
+                else {
+                    self.0[idx].a
+                }
+            }
+            else {
+                if self.0[0].a == 0 {
+                    self.0[0].b
+                }
+                else {
+                    self.0[0].a
+                }
+            };
+        if c.a == free || c.b == free {
+            Some(self.append(c))
+        }
+        else {
+            None
+        }
+    }
+    fn append(&self, c: &Component) -> Self {
+        let mut v = self.0.clone();
+        v.push(c.clone());
+        Self(v)
+    }
+    fn strength(&self) -> u64 {
+        self.0.iter().map(|c| c.a + c.b).sum()
+    }
+}
+
+fn build_all(input: &Vec<Component>) -> HashSet<Bridge> {
+    let mut set: HashSet<Bridge> = HashSet::new();
+    let mut queue: Vec<Bridge> = Vec::from_iter(
+        input.iter()
+        .filter(|c| c.a == 0 || c.b == 0)
+        .map(|c| Bridge::new(c))
+    );
+    queue.iter().for_each(|b| {set.insert(b.clone());});
+    while let Some(b) = queue.pop() {
+        let mut new:Vec<Bridge> = input.iter()
+            .flat_map(|c| b.maybe_append(c))
+            .filter(|nb| !set.contains(nb))
+            .collect();
+        for nb in new.iter() {
+            set.insert(nb.clone());
+        }
+        queue.append(&mut new);
+    }
+    set
+}
+
+fn part1(set: &HashSet<Bridge>) -> u64 {
+    set.iter().map(|b| b.strength()).max().unwrap()
+}
+
+fn part2(set: &HashSet<Bridge>) -> u64 {
+    let maxlen = set.iter().map(|b| b.0.len()).max().unwrap();
+    set.iter().filter(|b| b.0.len() == maxlen).map(|b| b.strength()).max().unwrap()
+}
+
+fn main() {
+    let input: Vec<Component> = read_input();
+    let set = build_all(&input);
+    println!("Part 1: {}", part1(&set));
+    println!("Part 2: {}", part2(&set));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use advent_lib::read::test_input;
+
+    #[test]
+    fn day24_test() {
+        let input: Vec<Component> = test_input(include_str!("day24.testinput"));
+        let set = build_all(&input);
+        assert_eq!(part1(&set), 31);
+        assert_eq!(part2(&set), 19);
+    }
+}
---
+++ src/bin/day25.rs
@@ -0,0 +1,226 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+use std::vec::Vec;
+use advent_lib::read::read_input;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum Bit {
+    Zero,
+    One,
+}
+
+impl FromStr for Bit {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.chars().next() {
+            Some('0') => Ok(Bit::Zero),
+            Some('1') => Ok(Bit::One),
+            _ => Err(()),
+        }
+    }
+}
+
+enum Input {
+    Begin(char),
+    Diagnostic(usize),
+    State(char),
+    If(Bit),
+    Write(Bit),
+    MoveLeft,
+    MoveRight,
+    NextState(char),
+    Blank,
+}
+
+impl FromStr for Input {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let words:Vec<&str> = s.split_whitespace().collect();
+        if words.len() == 0 {
+            return Ok(Input::Blank);
+        }
+        match (words[0], words[1]) {
+            ("Begin", "in") => {
+                let c = words[words.len()-1].chars().next().unwrap();
+                Ok(Input::Begin(c))
+            },
+            ("Perform", "a") => {
+                let steps = words[5].parse::<usize>().unwrap();
+                Ok(Input::Diagnostic(steps))
+            },
+            ("In", "state") => {
+                let c = words[words.len()-1].chars().next().unwrap();
+                Ok(Input::State(c))
+            },
+            ("If", "the") => {
+                let b = words[words.len()-1].parse::<Bit>().unwrap();
+                Ok(Input::If(b))
+            },
+            ("-", "Write") => {
+                let b = words[words.len()-1].parse::<Bit>().unwrap();
+                Ok(Input::Write(b))
+            },
+            ("-", "Move") => {
+                match words[words.len()-1] {
+                    "right." => Ok(Input::MoveRight),
+                    "left." => Ok(Input::MoveLeft),
+                    _ => Err(()),
+                }
+            },
+            ("-", "Continue") => {
+                let c = words[words.len()-1].chars().next().unwrap();
+                Ok(Input::NextState(c))
+            },
+            _ => Err(()),
+        }
+    }
+}
+
+enum Dir { Left, Right }
+
+struct Actions {
+    write: Bit,
+    mov: Dir,
+    next: char,
+}
+impl Actions {
+    fn from_iter(iter: &mut dyn Iterator<Item=&Input>) -> Self {
+        let mut write: Option<Bit> = None;
+        let mut mov: Option<Dir> = None;
+        let mut next: Option<char> = None;
+        while write.is_none() || mov.is_none() || next.is_none() {
+            match iter.next() {
+                Some(Input::Write(b)) => { write = Some(*b); },
+                Some(Input::MoveLeft) => { mov = Some(Dir::Left); },
+                Some(Input::MoveRight) => { mov = Some(Dir::Right); },
+                Some(Input::NextState(c)) => { next = Some(*c); },
+                _ => panic!(),
+            }
+        }
+        Self {
+            write: write.unwrap(),
+            mov: mov.unwrap(),
+            next: next.unwrap(),
+        }
+    }
+}
+
+struct State {
+    zero_actions: Actions,
+    one_actions: Actions,
+}
+impl State {
+    fn from_iter(iter: &mut dyn Iterator<Item=&Input>) -> Self {
+        let mut zero_actions: Option<Actions> = None;
+        let mut one_actions: Option<Actions> = None;
+        while zero_actions.is_none() || one_actions.is_none() {
+            match iter.next() {
+                Some(Input::If(Bit::Zero)) => {
+                    zero_actions = Some(Actions::from_iter(iter));
+                },
+                Some(Input::If(Bit::One)) => {
+                    one_actions = Some(Actions::from_iter(iter));
+                },
+                _ => panic!(),
+            }
+        }
+        Self {
+            zero_actions: zero_actions.unwrap(),
+            one_actions: one_actions.unwrap(),
+        }
+    }
+}
+
+struct Program {
+    initial_state: char,
+    states: HashMap<char, State>,
+    checksum_step: usize,
+}
+impl Program {
+    fn from_input(input: &Vec<Input>) -> Self {
+        let mut iter = input.iter().peekable();
+        let mut initial_state: Option<char> = None;
+        let mut checksum_step: Option<usize> = None;
+        let mut states: HashMap<char, State> = HashMap::new();
+
+        // top level
+        while let Some(row) = iter.next() {
+            match row {
+                Input::Begin(c) => {
+                    initial_state = Some(*c);
+                },
+                Input::Diagnostic(s) => {
+                    checksum_step = Some(*s);
+                },
+                Input::State(s) => {
+                    states.insert(*s, State::from_iter(&mut iter));
+                },
+                Input::Blank => {},
+                _ => panic!(),
+            }
+        }
+
+        Self {
+            initial_state: initial_state.unwrap(),
+            states,
+            checksum_step: checksum_step.unwrap(),
+        }
+    }
+}
+
+struct TuringMachine {
+    tape: HashMap<i64, Bit>,
+    program: Program,
+    state: char,
+    cursor: i64,
+}
+impl TuringMachine {
+    fn from_input(input: &Vec<Input>) -> Self {
+        let program = Program::from_input(input);
+        Self {
+            tape: HashMap::new(),
+            state: program.initial_state,
+            program,
+            cursor: 0,
+        }
+    }
+    fn step(&mut self) {
+        let state = &self.program.states[&self.state];
+        let val = self.tape.get(&self.cursor).unwrap_or(&Bit::Zero);
+        let actions = match val {
+            Bit::Zero => &state.zero_actions,
+            Bit::One => &state.one_actions,
+        };
+        self.tape.insert(self.cursor, actions.write);
+        self.cursor += match actions.mov {
+            Dir::Right => 1,
+            Dir::Left => -1,
+        };
+        self.state = actions.next;
+    }
+}
+
+fn part1(input: &Vec<Input>) -> usize {
+    let mut tm = TuringMachine::from_input(input);
+    for _ in 0..tm.program.checksum_step {
+        tm.step();
+    }
+    tm.tape.values().filter(|&&v| v == Bit::One).count()
+}
+
+fn main() {
+    let input: Vec<Input> = read_input();
+    println!("Part 1: {}", part1(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use advent_lib::read::test_input;
+
+    #[test]
+    fn day25_test() {
+        let input: Vec<Input> = test_input(include_str!("day25.testinput"));
+        assert_eq!(part1(&input), 3);
+    }
+}
